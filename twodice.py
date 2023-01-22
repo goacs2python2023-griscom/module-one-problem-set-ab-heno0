003ace5faec123a30e68dd3849b300d0397060ea
heno0 <96019650+heno0@users.noreply.github.com>
2023-01-22 14:00:19 -0800

    Update twodice.py

--- twodice.py
+++ twodice.py
@@ -1,8 +1,8 @@
 import random
 
 def dice():
-    d1 = random.randint(0,6)
-    d2 = random.randint(0,6)
+    d1 = random.randint(1,6)
+    d2 = random.randint(1,6)
     print(d1+d2)
     if 5 < (d1+d2) and (d1+d2) < 9:
         return "win"
